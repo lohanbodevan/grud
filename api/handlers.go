795c794f98b8dff685bdaf82a49d5f4adce82e23
Lohan Bodevan <lohan.bodevan@gmail.com>
2017-07-23 21:21:13 -0300

    Refactor in handlers to return correct status code

--- api/handlers.go
+++ api/handlers.go
@@ -15,17 +15,19 @@ func (a *Api) GetTVSeriesHandler(w http.ResponseWriter, r *http.Request) {
 	series, err := GetTVSeries(a.Repository)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	var response []byte
-
 	if len(series) > 0 {
 		response, err = json.Marshal(series)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 	} else {
 		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -37,52 +39,70 @@ func (a *Api) CreateTVSerieHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	err = CreateTVSerie(payload, a.Repository)
-	if err.Error() == VALIDATION_ERROR {
+	if err != nil && err.Error() == VALIDATION_ERROR {
 		w.WriteHeader(http.StatusBadRequest)
-	} else if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusCreated)
+		return
 	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (a *Api) UpdateTVSerieHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if vars["code"] == "" {
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	var payload TVSerie
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	err = UpdateTVSerie(vars["code"], payload, a.Repository)
-	if err.Error() == NOT_FOUND {
+	if err != nil && err.Error() == NOT_FOUND {
 		w.WriteHeader(http.StatusNotFound)
-	} else if err.Error() == VALIDATION_ERROR {
-		w.WriteHeader(http.StatusBadRequest)
-	} else if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
+		return
 	}
+	if err != nil && err.Error() == VALIDATION_ERROR {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 }
 
 func (a *Api) DeleteTVSerieHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if vars["code"] == "" {
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	err := DeleteTVSerie(vars["code"], a.Repository)
+	if err != nil && err.Error() == NOT_FOUND {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
+		return
 	}
+
+	w.WriteHeader(http.StatusOK)
 }
